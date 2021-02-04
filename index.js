8a2cf79c8564b0e6d09a940ca9a02dc8985891e2
Laura Heino <lhhein@gmail.com>
2021-02-04 17:52:35 +0100

    done

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
+const { update } = require('./models/Recipe.model');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
@@ -25,3 +26,84 @@ mongoose
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+  // Iteration 1 [schema] 
+
+   //Iteration 2
+   // function version
+  const createRecipe = (n) => {
+    return Recipe.create({
+      title: data[n].title,
+      level: data[n].level,
+      ingredients: data[n].ingredients,
+      cuisine: data[n].cuisine,
+      dishType: data[n].dishType,
+      image: data[n].image,
+      duration: data[n].duration,
+      creator: data[n].creator
+    })
+  }
+
+  // let createRecipe = Recipe.create({
+  //   title: data[0].title,
+  //   level: data[0].level,
+  //   ingredients: data[0].ingredients,
+  //   cuisine: data[0].cuisine,
+  //   dishType: data[0].dishType,
+  //   image: data[0].image,
+  //   duration: data[0].duration,
+  //   creator: data[0].creator
+  // })
+
+  createRecipe(2).then((result) => {
+    console.log(result)
+  })
+  .catch((err)=> {
+    console.log(err, 'something went wrong')
+  })
+
+//  Iteration 3
+
+let createSeveral = Recipe.insertMany(data)
+
+createSeveral.then((result) => {
+  result.forEach((item) => {
+    console.log(item.title)
+  })
+  })
+.catch((err) => {
+  console.log('error')
+})
+
+//  Iteration 4
+
+let updateRecipe = Recipe.findOneAndUpdate({ title: 'Rigatoni alla Genovese' }, {duration: 100})
+
+updateRecipe.then(() => {
+  console.log('successfully updated!')
+})
+.catch((err) => {
+  console.log(err, 'issue occurred')
+})
+
+// Iteration 5
+
+let removeRecipe = Recipe.deleteOne({title: 'Carrot Cake'})
+
+removeRecipe.then(() => {
+  console.log('Item removed!')
+})
+.catch((err) => {
+  console.log(err, 'Errored out')
+})
+
+//Iteration 6
+
+Promise.all([createRecipe, createSeveral, updateRecipe, removeRecipe]).then(() => {
+  mongoose.connection.close()
+  console.log('connection to db closed')
+})
+.catch((err) => {
+  console.log(err, 'error happened, could not close connection')
+})
+
